61a6d504b2f0eccb39cc43777847e6cb5573682c
OneNote Template Creator <onenote.template.creator@example.com>
2025-08-27 23:46:54 +0200

    Auto-commit: 2025-08-27 23:46:54

--- packages/web/src/pages/EmbeddedGraphiQLPage.tsx
+++ packages/web/src/pages/EmbeddedGraphiQLPage.tsx
@@ -1,13 +1,22 @@
 export function EmbeddedGraphiQLPage() {
   return (
-    <div className="w-full h-full overflow-hidden">
+    <div 
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: 1000,
+        background: 'white'
+      }}
+    >
       {/* Embedded GraphiQL iframe - full viewport */}
       <iframe
         src="http://localhost:4000/graphql"
-        className="w-full h-full border-none"
         style={{
-          width: '100vw',
-          height: '100vh',
+          width: '100%',
+          height: '100%',
           border: 'none',
           background: 'white',
           display: 'block'
